9845741ebece12096634c7adc5f0256d58a2af4b
ddwooten <ddwooten@rocketmail.com>
2014-11-04 15:23:14 -0800

    Finished code for p1. About to test

--- HW4/P1.py
+++ HW4/P1.py
@@ -29,8 +29,9 @@ OutputFile = 'output.dat'
 LogLevel = 0
 
 # This does some basic config on the log file
-logging.basicConfig( filename = LogFileName , format = "[%(levelname)8s] %(message)s" \
-    , filemode = 'w' , level = LogLevel )
+logging.basicConfig( filename = LogFileName , format = \
+    "[%(levelname)8s] %(message)s" , filemode =\
+     'w' , level = LogLevel )
 logging.debug( "This is the debug level reporting in" )
 logging.info( "This is the info level reporting in " )
 logging.warning( "This is the warning level reporting in" )
@@ -38,6 +39,33 @@ logging.error( "This is the error level reporting in" )
 logging.critical( "This is the critical level reporting in" )
 
 logging.debug( 'The number of samples being run is ' + str(N) )
+
+#Generate the random number arrays and scale them to the problem
+rand_x = np.random.rand(N)
+rand_y = np.random.rand(N)
+
+#Generate the logical vector of if the point are above or under
+#the curve
+above_a = rand_y > ( sqrt( 1 - ( rand_x**2 ) ) )
+above_b = rand_y > ( 1 / ( 1 + rand_x**2 ) )
+
+#Calculate pi approximation
+pi_approx_a = 4 * sum( above_a ) / N
+pi_approx_b = 4 * sum( above_b ) / N
+
+#Calculate the relative error
+rel_err_a = abs( ( pi_approx_a - np.pi ) / np.pi ) * 100
+rel_err_b = abs( ( pi_approx_b - np.pi ) / np.pi ) * 100
+
+#Print the results
+print 'The approximated value of pi for a is\n'
+print str( pi_approx_a )
+print 'With relative error \n'
+print str( rel_err_a )
+print 'The approximated value of pi for b is \n'
+print str( pi_approx_b )
+print 'With relative error \n'
+print str( rel_err_b )
 
 # Let the user know it has all ended
 print "Sn CODE END!!"
